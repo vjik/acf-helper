97bc7b66363dd51b65ab0726b40be09a95d917f2
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2021-04-08 12:30:23 +0300

    Add BlockFieldHelper::getValue()

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # ACF Helper Change Log
 
+## 0.4.3.1 — 08.04.2021
+
+- Enh: Add `BlockFieldHelper::getValue()` and support formatted values.
+
 ## 0.4.3 — 08.04.2021
 
 - Enh: Add `BlockFieldHelper`.
--- src/BlockFieldHelper.php
+++ src/BlockFieldHelper.php
@@ -9,9 +9,11 @@ use Vjik\SimpleTypeCaster\TypeCaster;
 use Yiisoft\Arrays\ArrayHelper;
 use Yiisoft\Strings\NumericHelper;
 
+use function array_key_exists;
 use function count;
 use function function_exists;
 use function is_array;
+use function is_string;
 
 final class BlockFieldHelper
 {
@@ -26,44 +28,70 @@ final class BlockFieldHelper
         return get_the_ID() . self::ID_SEPARATOR . acf_get_valid_post_id();
     }
 
-    public static function getStringOrNull(string $selector, string $fullBlockId): ?string
+    public static function getStringOrNull(string $selector, string $fullBlockId, bool $formatValue = true): ?string
     {
         return TypeCaster::toStringOrNull(
-            self::getBlockData($fullBlockId)[$selector] ?? null
+            self::getValue($selector, $fullBlockId, $formatValue)
         );
     }
 
-    private static function getBlockData(string $fullBlockId): array
+    /**
+     * @return mixed
+     */
+    public static function getValue(string $selector, string $fullBlockId, bool $formatValue = true)
     {
+        if (!function_exists('acf_maybe_get_field')) {
+            throw new RuntimeException('Function "acf_maybe_get_field" not found.');
+        }
+        if (!function_exists('acf_format_value')) {
+            throw new RuntimeException('Function "acf_format_value" not found.');
+        }
+
         $ids = explode(self::ID_SEPARATOR, $fullBlockId, 2);
         if (count($ids) !== 2) {
-            return [];
+            return null;
         }
 
         $postId = $ids[0];
         if (!NumericHelper::isInteger($postId)) {
-            return [];
+            return null;
         }
         $postId = (int)$postId;
 
         $blockId = $ids[1];
         if (empty($blockId)) {
-            return [];
+            return null;
         }
 
         $post = get_post($postId);
         if ($post === null) {
-            return [];
+            return null;
         }
 
         $blocks = parse_blocks($post->post_content);
         foreach ($blocks as $block) {
             if (is_array($block) && ArrayHelper::getValue($block, ['attrs', 'id']) === $blockId) {
-                $data = ArrayHelper::getValue($block, ['attrs', 'data'], []);
-                return is_array($data) ? $data : [];
+                $data = $block['attrs']['data'] ?? [];
+                if (!is_array($data) || !array_key_exists($selector, $data)) {
+                    return null;
+                }
+
+                $value = $data[$selector];
+
+                if ($formatValue) {
+                    $fieldId = $data['_' . $selector] ?? null;
+                    if (is_string($fieldId) && $fieldId !== '') {
+                        $field = acf_maybe_get_field($fieldId);
+                        if ($field !== false) {
+                            $value = acf_format_value($value, 0, $field);
+                        }
+                    }
+                }
+
+                return $value;
             }
         }
 
-        return [];
+        return null;
     }
 }
